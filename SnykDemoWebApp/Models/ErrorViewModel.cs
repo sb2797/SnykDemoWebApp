afb5b9d2ea52c251bef7d216f2557e319348aac9
André Stoll <andre.stoll@hotmail.com>
2022-01-31 22:45:45 +0100

    Initial project setup

---
+++ SnykDemoWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SnykDemoWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
