38a1c33af566b4eb506ff11da76c77916f2667a1
André Stoll <andre.stoll@hotmail.com>
2022-02-09 16:42:14 +0100

    Vulnerabilities page added

---
+++ SnykDemoWebApp/SQLiteContext.cs
@@ -0,0 +1,21 @@
+﻿using SnykDemoWebApp.Controllers;
+using Microsoft.EntityFrameworkCore;
+
+namespace SnykDemoWebApp;
+
+public class SQLiteContext : DbContext
+{
+    public DbSet<Vulnerability> Vulnerabilities { get; set; }
+    
+    public string DbPath { get; }
+
+    public SQLiteContext()
+    {
+        DbPath = System.IO.Path.Join(Directory.GetCurrentDirectory(), "VulDb.sqlite");
+    }
+    
+    // The following configures EF to create a Sqlite database file in the
+    // special "local" folder for your platform.
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={DbPath}");
+}
